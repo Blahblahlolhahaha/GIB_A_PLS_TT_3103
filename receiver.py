ca3df8f06c6401e63f5e54d9e1d610b407121a23
SherisseTJW <tanjingwensherisse@gmail.com>
2025-11-07 12:14:41 +0800

    feat: Log in receiver coz specs

--- receiver.py
+++ receiver.py
@@ -20,21 +20,30 @@ class Receiver:
             packets = self.gamenet.recv(timeout_ms=200)
 
             for data in packets:
-                ch, seq, send_timestamp, payload = data
+                ch, seq, send_timestamp, payload, recv_timestamp, half_rtt, retries = data
 
                 if ch == CH_RELIABLE:
                     reliable_str = "reliable"
                 else:
                     reliable_str = "unreliable"
 
-                # TODO: Print logs showing SeqNo, ChannelType, Timestamp,
-                #       Retransmissions, Packet Arrivals, RTT, etc
-                # FIX: Get Retransmission count + RTT
+                rtt = 2 * half_rtt
+
+                # Logging as requested in the specs
                 print(
-                    f"Received packet with seqNo: {seq} from {reliable_str} channel, sent at {send_timestamp} with payload: {payload}"
+                        f"Received packet with following details: \n    seqNo: {seq} \n" + 
+                        f"    Channel Type: {reliable_str} \n    Retries: {retries} \n"
+                        f"    Sent at {send_timestamp} \n    Received at: {recv_timestamp} \n"
+                        f"    Payload: {payload} \n    Estimated RTT: {rtt}"
                 )
+                print()
 
 
 if __name__ == "__main__":
-    receiver = Receiver("-m" in sys.argv)
+    if "-m" in sys.argv:
+        is_metric_mode = True
+    else:
+        is_metric_mode = False
+
+    receiver = Receiver(is_metric_mode)
     receiver.start()
